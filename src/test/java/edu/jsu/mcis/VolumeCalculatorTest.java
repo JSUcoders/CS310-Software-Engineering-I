e01966cb69dfb5b1cc7d58f1bc7a94b5a93a5933
Grady Houlditch <houlditchcode@gmail.com>
2016-02-02 13:18:30 -0600

    prelim unit test built
    added the following tests;
    
    testGetLength()
    testGetWidth()
    GetHight()
    GetVolume()

--- src/test/java/edu/jsu/mcis/VolumeCalculatorTest.java
+++ src/test/java/edu/jsu/mcis/VolumeCalculatorTest.java
@@ -5,7 +5,59 @@ import static org.junit.Assert.*;
 
 public class VolumeCalculatorTest {
     
+    private VolumeCalculator a;
+	
+	@Before 
+	public void setup(){
+		a = new VolumeCalculator();
     @Test
 	public void <method>{
 		
+		
 	}
+	
+	
+	@Test
+	public void testGetLength(){
+		boolean result = false;
+		a.VolumeCalculator(1,2,3);
+		
+		if(a.getLength == 1){
+			result = true;
+		} 
+		assertTrue(result);
+	}
+	@Test
+	public void testGetWidth(){
+		boolean result = false;
+		a.VolumeCalculator(1,2,3);
+		
+		if(a.getWidth == 2){
+			result = true;
+		} 
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testGetHight(){
+		boolean result = false;
+		a.VolumeCalculator(1,2,3);
+		
+		if(a.getHight == 3){
+			result = true;
+		} 
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testGetVolume(){
+		boolean result = false;
+		a.VolumeCalculator(1,2,3);
+		
+		if(a.getVolume == 3){
+			result = true;
+		} 
+		assertTrue(result);
+	}
+	
+}
