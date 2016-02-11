e27165c7fa0099ff279e24b3b471b818ffa48895
Grady Houlditch <houlditchcode@gmail.com>
2016-02-11 16:46:18 -0600

    update acceptance test template
    changed classpaths and created template for acceptance test for the
    first two features

--- runacceptance.bat
+++ runacceptance.bat
@@ -1,4 +1,4 @@
 cd acceptance
-javac -cp .;..\build\classes\main VolumeCalculatorKeywords.java
-java -cp .;..\build\classes\main;C:\Users\Grady\Software\robotframework-2.9.jar org.robotframework.RobotFramework VolumeCalculatorTests.txt
+javac -cp .;..\build\classes\main ArgsParserKeywords.java
+java -cp .;..\build\classes\main;C:\Users\Grady\Software\robotframework-2.9.jar org.robotframework.RobotFramework ArgsParserTests.txt
 cd ..
